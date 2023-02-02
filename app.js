aaa44968065ded1f6b2e3e929f25b583c4a6fd20
Steven <stevengf14@gmail.com>
2023-02-01 20:18:26 -0500

    Arrow functions

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const files = require("./files");
+const calculator = require("./calculator");
 
 console.log(files.num);
 files.writeFile;
+
+console.log(calculator.addition(5, 2));
+console.log(calculator.substraction(5, 2));
+console.log(calculator.duplicate(5));
---
+++ calculator.js
@@ -0,0 +1,10 @@
+// arrow functions
+module.exports = {
+  addition: (a, b) => {
+    return a + b;
+  },
+  substraction: (a, b) => a - b,
+  duplicate: (a) => {
+    return a * 2;
+  },
+};
