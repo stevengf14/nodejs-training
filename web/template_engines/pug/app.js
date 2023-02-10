4e6917031c2f886191ca9961aa2ae4d19301c70c
Steven <stevengf14@gmail.com>
2023-02-09 21:49:41 -0500

    pug & hbs

---
+++ web/template_engines/pug/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+let people = [
+  {
+    id: 1,
+    name: "Steven",
+  },
+  {
+    id: 2,
+    name: "Andres",
+  },
+  {
+    id: 1,
+    name: "Nicole",
+  },
+  {
+    id: 1,
+    name: "German",
+  },
+];
+
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("template", {
+    title: "pug",
+    message: "Mitocode Nodejs | Pug",
+    people,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Running on port 3000");
+});
